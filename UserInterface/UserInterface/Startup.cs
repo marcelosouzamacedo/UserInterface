38204c8b917eae18c288589f045646352a4a76e1
marcelosouzamacedo <marcelo.souzamacedo@gmail.com>
2017-10-04 19:17:37 -0300

    Inclusão do projeto MVC

---
+++ UserInterface/UserInterface/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UserInterface.Startup))]
+namespace UserInterface
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
